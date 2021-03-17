236d21d5c62a9f5f08d7bd9e7b33f3fbdb756833
Maou Yami <maou@yamitec.com>
2021-03-17 13:54:53 -0300

    send initial files

---
+++ README.md
@@ -0,0 +1,22 @@
+# YamiTec JunoSDK
+
+# Instalação
+
+para iniciar o projeto basta instalar apartir do composer  com o comando ```composer require yamitec/juno-sdk```
+
+# Exemplo 
+
+Para efetuar pagamentos com a JUNO_SDK basta instanciar a classe ```\YamiTec\JunoSDK```.
+
+o arquivo deve conter o seguinte conteúdo:
+
+```
+
+```
+# Retornos
+
+(Em DEV)
+
+# Agradecimento
+
+Equipe www.yamitec.com
---
+++ src/JunoSdk.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace YamiTec\JunoSDK;
+class ClientAttr {
+    public $clientId;
+    public $clientSecret = '';
+    public $base64_credentials = '';
+
+    public function setBase64_credentials(){
+
+    }
+}
+class JunoSDK
+{
+    public $clientAttr;
+    public function __construct(ClientAttr $clientAttr){
+        $this->clientAttr = $clientAttr;
+    }
+    public function Authorization(Array $data )
+    {
+        //$base64 = base64_encode($this->clientAttr->base64_credentials);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.juno.com.br/authorization-server/oauth/token");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $headers = [
+            'Content-Type: application/x-www-form-urlencoded;',
+            'Authorization: Basic ' . $this->clientAttr->base64_credentials . '',
+            'Host: api.fullprog.dev',
+            'grant_type=client_credentials&clientId=' . $this->clientAttr->clientId . '&clientSecret=' . $this->clientAttr->clientSecret . ''
+        ];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        $json = json_decode($server_output);
+        $status = $json->status;
+        if ($status == '401') {
+            $errorMessage = $json->error;
+            $ErroMensagem = $json->message;
+            $time = date('d/m/Y H:i', strtotime($json->timestamp));
+            echo 'Em ' . $time . ' ocorreu um erro. <br>Detalhes: ' . $errorMessage . ' Complemento: ' . $ErroMensagem;
+        } else {
+            if (isset($json->error)) {
+                echo $json->error . ' informa: ' . $json->error_description;
+            } else {
+                print  $server_output;
+            }
+        }
+    }
+}
