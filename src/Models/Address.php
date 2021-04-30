8add7dd946f9b4eadc70f78f1c1b2c019db40feb
Maou Yami <maou@yamitec.com>
2021-04-29 21:20:54 -0300

    make models Billing 'n Address update SDK methods

---
+++ src/Models/Address.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace YamiTec\JunoSDK\Models;
+
+class Address {
+    public $street;
+    public $number;
+    public $complement;
+    public $neighborhood;
+    public $city;
+    public $state;
+    public $postCode;
+    
+}
---
+++ src/Models/Billing.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace YamiTec\JunoSDK\Models;
+
+class Billing {
+    public $name;
+    public $document;
+    public $email;
+    public Address $address;
+
+}
